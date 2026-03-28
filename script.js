f991325620132c286d7649163ca37a7b3eedc4e9
N1covv <nico.veron09@gmail.com>
2026-03-28 00:09:38 -0300

    cambios

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const countdown = document.getElementById("countdown");
-
-const eventDate = new Date("Oct 20, 2026 21:00:00").getTime();
+// ⏳ CONTADOR
+const eventDate = new Date(2026, 4, 16, 21, 30, 0).getTime();
 
 setInterval(() => {
   const now = new Date().getTime();
   const distance = eventDate - now;
 
+  if (distance < 0) {
+    document.querySelector(".countdown-box").innerHTML = "🎉 ¡Llegó el gran día!";
+    return;
+  }
+
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
   const minutes = Math.floor((distance / 1000 / 60) % 60);
+  const seconds = Math.floor((distance / 1000) % 60);
 
-  countdown.innerHTML = `${days} días ${hours} horas ${minutes} min`;
+  document.getElementById("DIAS").innerText = days;
+  document.getElementById("HORAS").innerText = hours;
+  document.getElementById("MINUTOS").innerText = minutes;
+  document.getElementById("SEGUNDOS").innerText = seconds;
+
 }, 1000);
